e5aca47cd41128d0ec087b59ebb9009de2e18d7b
len201 <cctnsw@gmail.com>
2021-03-14 15:53:03 -0400

    changed greeting

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 //import express lib
-const express = require('express'); 
+const express = require('express');
 
 //create 1st express app
 const app = express();
 
 //create 1st express route handler
 app.get('/', (req, res) => {
-  res.send( { h1: 'there' });
+  res.send({ Bye: 'buddy' });
 });
 
 // this line is for heroku:
-// if Heroku already defined an envir var, sign that var to port 
+// if Heroku already defined an envir var, sign that var to port
 // otherwise by default just use the value of 5000
 const PORT = process.env.PORT || 5000;
 
