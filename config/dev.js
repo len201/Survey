256f30af925fd8484f65b69de4dadf484d59ea6c
len201 <cctnsw@gmail.com>
2021-03-22 17:39:37 -0400

    2nd heroku deploy

--- config/dev.js
+++ config/dev.js
@@ -3,5 +3,7 @@ module.exports = {
     googleClientID: '774380716330-htgqvshs7h3p513n7cp1kiibbug51j77.apps.googleusercontent.com',
     googleClientSecret: 'M_TvZfs3hk4hDUE72CM6AqW1',
     mongoURI: 'mongodb+srv://dbUser:fM0nfbzOKkJXSIs5@survey.jr1se.mongodb.net/survey?retryWrites=true&w=majority',
-    cookieKey: 'ihfkjastesghpwdst'
+    cookieKey: 'ihfkjastesghpwdst',
+    stripePublishableKey: 'pk_test_51IXthwD8TeKYwB5muwGOG6RVBBFx2jObrISCTDXJIFGEvdSJSdYHnwDB6wrquARCyO9DXeVLFwigFqKN5cjHyOkY00jjtkgm8Q',
+    stripeSecretKey: 'pk_test_51IXthwD8TeKYwB5muwGOG6RVBBFx2jObrISCTDXJIFGEvdSJSdYHnwDB6wrquARCyO9DXeVLFwigFqKN5cjHyOkY00jjtkgm8Q'
 };
