c45ad1ad99278c433fc83fef6b35d3c98ddc8231
len201 <cctnsw@gmail.com>
2021-03-23 23:49:30 -0400

    add client side app

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client','build','index.html'));
+    });
+}
+
 // this line is for heroku:
 // if Heroku already defined an envir var, sign that var to port
 // otherwise by default just use the value of 5000
