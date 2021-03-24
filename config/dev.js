48b08f7c4e2a96b231f1f5713c60d9104e4de6fa
len201 <cctnsw@gmail.com>
2021-03-24 00:07:59 -0400

    true 2nd heroku deploy

--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,5 @@ module.exports = {
     mongoURI: 'mongodb+srv://dbUser:fM0nfbzOKkJXSIs5@survey.jr1se.mongodb.net/survey?retryWrites=true&w=majority',
     cookieKey: 'ihfkjastesghpwdst',
     stripePublishableKey: 'pk_test_51IXthwD8TeKYwB5muwGOG6RVBBFx2jObrISCTDXJIFGEvdSJSdYHnwDB6wrquARCyO9DXeVLFwigFqKN5cjHyOkY00jjtkgm8Q',
-    stripeSecretKey: 'pk_test_51IXthwD8TeKYwB5muwGOG6RVBBFx2jObrISCTDXJIFGEvdSJSdYHnwDB6wrquARCyO9DXeVLFwigFqKN5cjHyOkY00jjtkgm8Q'
+    stripeSecretKey: 'sk_test_51IXthwD8TeKYwB5mOClcYluiSuVS5eoCE3aPMdgl0C8rGmWCpzQrpPELQYuyMcy6E4cw0OJHz8vktZYQPmz813i000EnTqX4I0'
 };
