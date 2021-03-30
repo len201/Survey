c66d1cb32b84a6d89d2b978bd8a26d16011aea11
len201 <cctnsw@gmail.com>
2021-03-29 21:06:48 -0400

    debug stripe key

--- config/dev.js
+++ config/dev.js
@@ -5,5 +5,6 @@ module.exports = {
     mongoURI: 'mongodb+srv://dbUser:fM0nfbzOKkJXSIs5@survey.jr1se.mongodb.net/survey?retryWrites=true&w=majority',
     cookieKey: 'ihfkjastesghpwdst',
     stripePublishableKey: 'pk_test_51IXthwD8TeKYwB5muwGOG6RVBBFx2jObrISCTDXJIFGEvdSJSdYHnwDB6wrquARCyO9DXeVLFwigFqKN5cjHyOkY00jjtkgm8Q',
-    stripeSecretKey: 'sk_test_51IXthwD8TeKYwB5mOClcYluiSuVS5eoCE3aPMdgl0C8rGmWCpzQrpPELQYuyMcy6E4cw0OJHz8vktZYQPmz813i000EnTqX4I0'
+    stripeSecretKey: 'sk_test_51IXthwD8TeKYwB5mOClcYluiSuVS5eoCE3aPMdgl0C8rGmWCpzQrpPELQYuyMcy6E4cw0OJHz8vktZYQPmz813i000EnTqX4I0',
+    sendGridKey: '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'
 };
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -20,9 +20,3 @@ module.exports = app => {
         });
     });
 };
-
-
-
-//authenticated
-
-//has credit
