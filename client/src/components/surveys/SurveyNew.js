2544b5099cf40b44c41738e44bd7eaa5ee2932aa
len201 <cctnsw@gmail.com>
2021-04-04 20:37:27 -0400

    build survey form review

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,15 +1,36 @@
-//SurveyNew shows survey form and survey review
+//SurveyNew shows both survey form and survey review
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+    state = { showFormReview: false }; //set initial state false
+
+    renderContent() {
+        if (this.state.showFormReview){
+            return (
+                <SurveyFormReview
+                    onCancel={() => this.setState({ showFormReview: false })}
+            />
+            );
+        }
+        return (
+            <SurveyForm
+                onSurveySubmit={() => this.setState({ showFormReview: true })}
+            />
+        );
+    }
+
     render() {
         return (
             <div>
-                <SurveyForm />
+                {this.renderContent()}
             </div>
         );
     }
 }
 
-export default SurveyNew;
+export default reduxForm({
+    form: 'surveyForm'
+})(SurveyNew);
